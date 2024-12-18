988a92589d18309926f73c6a04f3682c55292dfe
권성준 <128343377+kwonjonny@users.noreply.github.com>
2024-12-18 16:13:21 +0900

    Feat: Throws
    - Throws 에 대한 실습

---
+++ nado-coding/src/chap_11/_07_Throws.java
@@ -0,0 +1,33 @@
+package chap_11;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Throws
+ */
+public class _07_Throws {
+    public static void main(String[] args) {
+
+        // 매우 좋지 않은 경우임
+        try {
+            writeFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("메인 메소드에서 해결할게요.");
+        }
+    }
+
+    public static void writeFile() throws IOException {
+//        try {
+//            FileWriter writer = new FileWriter("test.txt");
+//            throw new IOException("파일 쓰기에 실패 했어요.");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            System.out.println("WriteFile 메소드 내에서 자체 해결했어요.");
+//        }
+
+        FileWriter writer = new FileWriter("test.txt");
+        throw new IOException("파일 쓰기에 실패 했어요.");
+    }
+}
